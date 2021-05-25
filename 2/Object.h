835ca653480764e592d22244ed4a74be6b59f286
chunchunyd <hawangyudong@outlook.com>
2021-05-26 04:42:01 +0800

    v_4.1:做完前两个subtask

--- 2/Object.h
+++ 2/Object.h
@@ -1,13 +1,15 @@
 #pragma once
 #include <list>
 #include <iostream>
+#include <memory>
 
 #include "CustomClass.h"
 
 class Content
 {
+public:
 	virtual void output(std::ostream& out){ //virtual function for output
-		std::cout << "the operator is not supported for this type." << std::endl;
+		std::cout << "the operator is not supported for this type." << std::endl;	//沒有重寫覆蓋説明不支持
 	}
 };
 
@@ -17,20 +19,28 @@ private:
 	int x;
 public:
 	IntContent(int _x): x(_x) {}
-	void output(const std::ostream& out){ //???
+	void output(std::ostream& out){ //???-->不能加const
 		out << x;
 	}
+	void operator+=(int y){
+		x += y;
+	}
 };
+
 class StringContent: public Content
 {
 private:
 	std::string x;
 public:
 	StringContent(std::string _x): x(_x) {}
-	void output(const std::ostream& out){  //???
+	void output(std::ostream& out){ 
 		out << x;
 	}
+	void operator+=(std::string y){
+		x += y;
+	}
 };
+
 // The following codes are not working
 // class VectorContent: public Content
 // {
@@ -39,6 +49,8 @@ public:
 // public:
 // 	VectorContent(const std::vector<Object>& _x): x(_x) {}
 // };
+
+
 class CustomContent: public Content
 {
 private:
@@ -47,26 +59,33 @@ public:
 	CustomContent(const CustomClass& _x): x(_x) {}
 };
 
+
+
 class Object
 {
 private:
-	Content* pt;
+	std::shared_ptr<Content> pt;
 
 public:
+//构造函数	
 	Object() {}
 	Object(int x) {
-		pt = new int(x); // where to delete? maybe use std::shared_ptr?
+		pt = std::make_shared<IntContent>(x); // where to delete? maybe use std::shared_ptr?
 	}
-	Object(const std::string &x){
-		pt = new StringContent(x);
+	Object(const std::string &x):pt(new StringContent(x)){
+	}
+	Object(const CustomClass &x):pt(std::make_shared<CustomContent>(x)){
 	}
 	// The following codes are not working
 	// Object(const std::vector<Object> &x){
 	// 	pt = new VectorContent(x);
 	// }
-	Object(const CustomClass &x){
-		pt = new CustomContent(x);
+	
+//析构函数
+	~Object(){
+		pt.reset();
 	}
+
 
 	friend std::ostream& operator<<(std::ostream& out, const Object& obj) {
 		obj.pt->output(out);
@@ -74,11 +93,11 @@ public:
 	}
 
 	Object& operator+=(int y){
-		pt->operator+=(y);
+		*std::dynamic_pointer_cast<IntContent>(pt)+=y;			//基类、派生类的智能指针相互转化
 		return *this;
 	}
 	Object& operator+=(const std::string &y){
-		pt->operator+=(y);
+		std::dynamic_pointer_cast<StringContent>(pt)->operator+=(y);
 		return *this;
 	}
 
--- 2/main.cpp
+++ 2/main.cpp
@@ -41,42 +41,42 @@ int main()
     }
     CustomClass::detect_leak();
 
-    {
-        //subtask 3
-        std::vector<Object> arr;
-        arr.push_back(Object(1));
-        arr.push_back(Object("text"));
-        arr.push_back(Object(CustomClass("obj3")));
-        std::cout << arr[0] << " " << arr[1] << std::endl; //1 text
-        arr[0] = arr[1];
-        std::cout << arr[0] << " " << arr[1] << std::endl; //text text
-        arr[1] += std::string("1");
-        std::cout << arr[0] << " " << arr[1] << std::endl; //text1 text1
-        arr[0] = arr[2];
-        std::cout << arr[1] << std::endl; //text1
-    }
-    CustomClass::detect_leak();
+    // {
+    //     //subtask 3
+    //     std::vector<Object> arr;
+    //     arr.push_back(Object(1));
+    //     arr.push_back(Object("text"));
+    //     arr.push_back(Object(CustomClass("obj3")));
+    //     std::cout << arr[0] << " " << arr[1] << std::endl; //1 text
+    //     arr[0] = arr[1];
+    //     std::cout << arr[0] << " " << arr[1] << std::endl; //text text
+    //     arr[1] += std::string("1");
+    //     std::cout << arr[0] << " " << arr[1] << std::endl; //text1 text1
+    //     arr[0] = arr[2];
+    //     std::cout << arr[1] << std::endl; //text1
+    // }
+    // CustomClass::detect_leak();
 
-    {
-        //subtask 4
-        std::vector<Object> inner_arr1 = {Object(1), Object("text1"), CustomClass("obj4")};
-        std::vector<Object> inner_arr2 = {Object(2), Object("text2")};
-        std::vector<Object> arr = {Object(inner_arr1), Object(inner_arr2), CustomClass("obj5")};
+    // {
+    //     //subtask 4
+    //     std::vector<Object> inner_arr1 = {Object(1), Object("text1"), CustomClass("obj4")};
+    //     std::vector<Object> inner_arr2 = {Object(2), Object("text2")};
+    //     std::vector<Object> arr = {Object(inner_arr1), Object(inner_arr2), CustomClass("obj5")};
 
-        std::cout << arr[0][0] << " " << arr[0][1] << std::endl; // 1 text1
-        std::cout << arr[1][0] << " " << arr[1][1] << std::endl; // 2 text2
+    //     std::cout << arr[0][0] << " " << arr[0][1] << std::endl; // 1 text1
+    //     std::cout << arr[1][0] << " " << arr[1][1] << std::endl; // 2 text2
 
-        arr[1] = arr[0];
-        std::cout << arr[1][0] << " " << arr[1][1] << std::endl; // 1 text1
+    //     arr[1] = arr[0];
+    //     std::cout << arr[1][0] << " " << arr[1][1] << std::endl; // 1 text1
 
-        arr[0][0] += 2;
-        std::cout << arr[0][0] << " " << arr[1][0] << std::endl; // 3 3
+    //     arr[0][0] += 2;
+    //     std::cout << arr[0][0] << " " << arr[1][0] << std::endl; // 3 3
 
-        arr[0][1] = arr[1][0]; // arr[1][1] and arr[0][1] is the same Object
-        std::cout << arr[0][1] << " " << arr[1][1] << std::endl; // 3 3
+    //     arr[0][1] = arr[1][0]; // arr[1][1] and arr[0][1] is the same Object
+    //     std::cout << arr[0][1] << " " << arr[1][1] << std::endl; // 3 3
 
-        arr[0][0] = arr[2];
-    }
+    //     arr[0][0] = arr[2];
+    // }
     CustomClass::detect_leak();
 
     return 0;
